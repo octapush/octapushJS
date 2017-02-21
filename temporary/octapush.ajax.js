7e80d9c5955b008cec693afec3420c48d25bf52b
Fadhly Permata <fadh325@yahoo.com>
2017-02-21 16:00:06 +0700

    Finish octapush.ajax.js core

--- temporary/octapush.ajax.js
+++ temporary/octapush.ajax.js
@@ -20,6 +20,11 @@
  * - RESTFULL test URL: https://reqres.in/api/users
  */
 
+/**
+ * TODO: 
+ * - ADD parameterize())
+ */
+
 (function (w) {
     'use strict';
     if (!w.octapushJS || !w._o_) {
@@ -30,12 +35,12 @@
         const version = '1.7.02.13';
 
         _o_.string = Object.assign(_o_.utility.ifNull(_o_.string, {}), {
-            convertToQueryString: function (object) {
+            convertToQueryString: function(object) {
                 return !_o_.compare.isJsonObject(object) ?
                     object :
                     (
-                        Object.keys(object).reduce(function (acc, val) {
-                            var prefix = !acc ? '' : [acc, '&'].join('');
+                        Object.keys(object).reduce(function(acc, val) {
+                            var prefix = !acc ? '' : [acc, '&'].join(''); 
                             return [
                                 prefix,
                                 encodeURIComponent(val),
@@ -49,31 +54,28 @@
 
         _o_.ajax = Object.assign(_o_.utility.ifNull(_o_.ajax, {}), {
             request: function (params) {
-                if (_o_.compare.isNullOrEmpty(params) || _o_.compare.isNullOrEmpty(params.url)) return;
+                var xhr = null;
 
                 params = {
                     url: params.url || null,
                     data: params.data || null,
                     method: _o_.utility.ifNull(params.method, 'GET'),
+                    async: _o_.utility.ifNull(params.async, false),
                     success: params.success || null,
-                    error: params.error || null,
-                    headers: params.headers || {},
-                    // headers: _o_.utility.ifNull(params.headers, {
-                    //     'Content-Type': 'application/x-www-form-urlencoded'
-                    // }),
-                    withCredentials: params.withCredentials || false,
-                    dataType: 'TEXT'
+                    error: params.error || null, 
+                    headers: _o_.utility.ifNull(params.headers, {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    })
                 };
 
-                var xhr = null;
+                if (_o_.compare.isNullOrEmpty(params)) return;
 
-                // BOF: CROSS BROWSER SUPPORT
-                // Chrome/Firefox/Opera/Safari/IE10+
+                // BOF : create XHR
+                // Chrome/Firefox/Opera/Safari
                 if (_o_.compare.isDefined(_o_.utility.getType(XMLHttpRequest))) {
                     xhr = new XMLHttpRequest();
 
                 } else {
-                    // check for XDomainRequest object
                     if (_o_.compare.isDefined(_o_.getType(XDomainRequest))) {
                         xhr = new XDomainRequest();
 
@@ -87,9 +89,10 @@
                             "Microsoft.XmlHttp"
                         ];
 
-                        // create xhr using higher version
                         for (var i = 0; i < version.length; i++) {
                             try {
+                                xhr = new ActiveXObject(version[i]);
+                                break;
 
                             } catch (e) {
                                 if (!_o_.compare.isNullOrEmpty(params.error) && _o_.compare.isFunction(params.error))
@@ -98,28 +101,35 @@
                         }
                     }
                 }
-                // EOF: CROSS BROWSER SUPPORT
+                // EOF : create XHR
 
+                // CORS support handler
                 if (params.method.toLowerCase() === 'get')
                     params.url = params.url + (_o_.compare.isNullOrEmpty(params.data) ? '' : '?' + params.data);
 
-                xhr.open(params.method, params.url, true);
+                if (("withCredentials" in xhr) && (_o_.compare.isDefined(_o_.utility.getType(XMLHttpRequest)))) {
+                    xhr.open(params.method, params.url, true);
 
+                } else {
+                    xhr.open(params.method, params.url);
+                }
 
-                // use credentials
-                xhr.withCredentials = params.withCredentials;
-
-                // set headers (preflight mode)
-                _o_.utility.each(params.headers, function(key, val) {
-                    if (val)
-                        xhr.setRequestHeader(key, val);
+                // set headers
+                //xhr.setRequestHeader('Content-Type', params.type);
+                if (_o_.compare.isNullOrEmpty(params.headers['X-Requested-With']))
+                    params.headers = Object.assign(_o_.utility.ifNull(params.headers, {}), {
+                        'X-Requested-With': 'XMLHttpRequest'
+                    });
+                    
+                Object.keys(params.headers).forEach(function(key) {
+                    (params.headers[key] && xhr.setRequestHeader(key, params.headers[key]))
                 });
 
                 xhr.send(params.method.toLowerCase() === 'get' ? null : params.data);
 
-                xhr.onreadystatechange = function() {
+                xhr.onreadystatechange = function () {
                     if (xhr.readyState === XMLHttpRequest.DONE) {
-                        if (xhr.status === 200 || xhr.status === 201 || xhr.status === 200) {
+                        if (xhr.status === 200 || xhr.status === 201 || xhr.status === 204) {
                             if (!_o_.compare.isNullOrEmpty(params.success) && _o_.compare.isFunction(params.success))
                                 params.success(xhr);
 
@@ -129,6 +139,54 @@
                         }
                     }
                 };
+            },
+
+            get: function (url, data, success, error) {
+                _o_.ajax.request({
+                    url: url,
+                    data: data,
+                    method: 'GET',
+                    success: success,
+                    error: error
+                });
+            },
+
+            post: function (url, data, success, error) {
+                _o_.ajax.request({
+                    url: url,
+                    data: data,
+                    method: 'POST',
+                    success: success,
+                    error: error
+                });
+            },
+
+            put: function (url, data, success, error) {
+                _o_.ajax.request({
+                    url: url,
+                    data: data,
+                    method: 'PUT',
+                    success: success,
+                    error: error
+                });
+            },
+
+            delete: function (url, success, error) {
+                _o_.ajax.request({
+                    url: url,
+                    success: success,
+                    error: error
+                });
+            },
+            
+            patch: function (url, data, success, error) {
+                _o_.ajax.request({
+                    url: url,
+                    data: data,
+                    method: 'PATCH',
+                    success: success,
+                    error: error
+                });
             }
         });
     }
